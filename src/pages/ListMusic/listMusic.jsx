1e8a65bda83f53551a25df7f0be365f5b51f7f5c
wlisses <wlissesfernando285@gmail.com>
2024-03-14 20:10:34 -0300

    adding elements

--- src/pages/ListMusic/listMusic.jsx
+++ src/pages/ListMusic/listMusic.jsx
@@ -2,20 +2,22 @@ import { useContext } from "react";
 import "./listMusic.css";
 import MyContext from "../../context/context";
 import ShowLoad from "../../components/ShowLoad/showLoad";
+import iconFavicon from "./images/spotyfree_favidon.png";
 
 function ListMusic() {
 
   const { musics } = useContext(MyContext);
+
   return (
-    <div>
+    <div className="container_manager_list_music">
       {musics.length > 0 ?
         musics.map((music) => (
           <section key={music.title}>
-            <img src={music.music} alt={music.title} />
+            <img id="icon_aside" src={iconFavicon} alt="apotyfree" />
+            <img src={music.image} alt={music.title} />
             <aside>
               <h2>{music.title}</h2>
               <h3>{music.category}</h3>
-              <p>{music.description}</p>
             </aside>
           </section>
         ))
