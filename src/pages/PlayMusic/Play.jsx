d3049114f79fc72dccdcb4b648eb123f2857ca71
wlisses <wlissesfernando285@gmail.com>
2024-03-16 06:41:39 -0300

    added animation in description

--- src/pages/PlayMusic/Play.jsx
+++ src/pages/PlayMusic/Play.jsx
@@ -1,39 +1,21 @@
 import PropTypes from "prop-types";
-// import { useState } from "react";
 
 function Play({
   returnIcon,
-  spotyfree,
   clickedMusic,
   handleAudioValue,
   showTimeMusic,
   isPlay,
   audioRef,
 }) {
-  // const [textPosition, setTextPosition] = useState({
-  //   init: 0,
-  //   end: 30
-  // })
-
-  // function setValue() {
-  //   const lengthText = clickedMusic.description.length;
-  //   setInterval(() => {
-  //     if (textPosition.end < lengthText) {
-  //       setTextPosition((prev) => ({ ...prev, end: textPosition.end + 1 }));
-  //       console.log(textPosition.end)
-  //     }
-  //   }, 1000);
-  // }
-
 
   return (
     <div className="container_object_music">
       <header className="container_header_play_music">
         <img src={returnIcon} alt="voltar para a página anterior" />
         <div id="container_text_description">
-          <span id="text_description">{clickedMusic.description.substring(0, 30)}</span>
+          <span id="text_description">{clickedMusic.description}</span>
         </div>
-        <img src={spotyfree} alt="spoty free" />
       </header>
       <img
         id="image_play_music"
@@ -56,7 +38,6 @@ function Play({
 Play.propTypes = {
   returnIcon: PropTypes.object.isRequired,
   musics: PropTypes.array.isRequired,
-  spotyfree: PropTypes.object.isRequired,
   clickedMusic: PropTypes.bool.isRequired,
   handleAudioValue: PropTypes.func.isRequired,
   showTimeMusic: PropTypes.func.isRequired,
